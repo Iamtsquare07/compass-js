26e6a7837ade40e067fd6143e28d962c9719a157
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-11-09 22:59:32 +0000

    Add files via upload

--- compass.js
+++ compass.js
@@ -11,6 +11,7 @@ function handleOrientation(event) {
 function updateCompass(heading) {
     var compassElement = document.querySelector('.circle');
     var headingElement = document.getElementById('heading');
+    var positionElement = document.getElementById('position');
 
     // Convert the heading to degrees
     var degrees = Math.round(heading);
@@ -18,6 +19,19 @@ function updateCompass(heading) {
     // Update the displayed heading
     headingElement.textContent = degrees;
 
+    // Calculate the cardinal direction
+    var cardinalDirection = getCardinalDirection(degrees);
+
+    // Update the position field
+    positionElement.textContent = cardinalDirection;
+
     // Rotate the compass circle to match the heading
     compassElement.style.transform = 'rotate(' + degrees + 'deg)';
+}
+
+// Function to get the cardinal direction based on the heading
+function getCardinalDirection(heading) {
+    var directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'EW', 'NE', 'SE', 'SW'];
+    var index = Math.round(heading / 45) % 12;
+    return directions[index];
 }
