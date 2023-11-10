91306808f0a7e785aacfa87e47ae20823a6fd59f
Victor Tyavbee <68505976+Iamtsquare07@users.noreply.github.com>
2023-11-10 01:57:18 +0000

    Add files via upload

--- compass.js
+++ compass.js
@@ -1,37 +1,55 @@
-window.addEventListener('deviceorientation', handleOrientation);
+window.addEventListener("deviceorientation", handleOrientation);
 
 function handleOrientation(event) {
-    if (event.webkitCompassHeading) {
-        updateCompass(event.webkitCompassHeading);
-    } else {
-        updateCompass(event.alpha);
-    }
+  if (event.webkitCompassHeading) {
+    // IOS Devices
+    updateCompass(event.webkitCompassHeading);
+  } else if (event.alpha) {
+    // Android devices
+    updateCompass(event.alpha);
+  } else {
+    alert(
+      "It seems like your device does not have the sensors needed to run this application. Sorry for the inconvenience"
+    );
+  }
 }
 
 function updateCompass(heading) {
-    var compassElement = document.querySelector('.circle');
-    var headingElement = document.getElementById('heading');
-    var positionElement = document.getElementById('position');
+  var compassElement = document.querySelector(".circle");
+  var headingElement = document.getElementById("heading");
+  var positionElement = document.getElementById("position");
 
-    // Convert the heading to degrees
-    var degrees = Math.round(heading);
+  // Convert the heading to degrees
+  var degrees = Math.round(heading);
 
-    headingElement.textContent = degrees;
+  headingElement.textContent = degrees;
 
-    // Rotate the compass circle to match the heading
-    compassElement.style.transform = 'rotate(' + degrees + 'deg)';
-    
-    // Calculate the cardinal direction
-    var cardinalDirection = getCardinalDirection(degrees);
+  // Rotate the compass circle to match the heading
+  compassElement.style.transform = "rotate(" + degrees + "deg)";
 
-    // Update the position field
-    positionElement.textContent = cardinalDirection;
+  // Calculate the cardinal direction
+  var cardinalDirection = getCardinalDirection(degrees);
+
+  // Update the position field
+  positionElement.textContent = cardinalDirection;
 }
-
 
 // Function to get the cardinal direction
 function getCardinalDirection(heading) {
-    var directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'EW', 'ENE', 'ESE', 'WSW'];
-    var index = Math.round(heading / 45) % 12;
-    return directions[index];
+  var directions = [
+    "N",
+    "NE",
+    "E",
+    "SE",
+    "S",
+    "SW",
+    "W",
+    "NW",
+    "EW",
+    "ENE",
+    "ESE",
+    "WSW",
+  ];
+  var index = Math.round(heading / 45) % 12;
+  return directions[index];
 }
